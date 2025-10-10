abbd96ffc5c43ac4d2793844a192cd797f245443
Evgenii Klimov <eugene.dev@lipklim.org>
2025-10-10 11:57:07 +0100

    Add missing parent node in query.
    * queries/graphql/formatter.scm: input_object_type_definition node can
    not have input_value_definition node as its direct child [1].
    input_fields_definition should be put in between.
    
    [1] https://github.com/bkegley/tree-sitter-graphql/blob/5e66e961eee421786bdda8495ed1db045e06b5fe/grammar.js#L234

--- queries/graphql/formatter.scm
+++ queries/graphql/formatter.scm
@@ -2,5 +2,5 @@
 (fields_definition (field_definition) @field_definition)
 (fields_definition (field_definition (name) @name)) 
 (input_object_type_definition) @fields_definition
-(input_object_type_definition (input_value_definition) @field_definition)
-(input_object_type_definition (input_value_definition (name) @name)) 
+(input_object_type_definition (input_fields_definition (input_value_definition)) @field_definition)
+(input_object_type_definition (input_fields_definition (input_value_definition (name) @name))) 
